3905a30220e84b728ef4788cf0cd597307e04b27
edunafiu713-debug <edunafiu713@gmail.com>
2025-12-29 09:52:31 +0600

    Add files via upload

---
+++ script 1.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    mobileMenuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+
+    // Optional: Close mobile menu when clicking a link
+    const mobileLinks = mobileMenu.querySelectorAll('a');
+    mobileLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            mobileMenu.classList.add('hidden');
+        });
+    });
+});
